2ff33060ad061fe9bfebea7dae15d6a1d0fb0086
Tang Ziya <tcztzy@gmail.com>
2018-10-01 16:20:06 +0800

    dns

--- sunshinesocks/dns.py
+++ sunshinesocks/dns.py
@@ -19,6 +19,38 @@ except ImportError:
 from sunshinesocks.utils import lru_cache
 
 
+# rfc1035
+# format
+# +---------------------+
+# |        Header       |
+# +---------------------+
+# |       Question      | the question for the name server
+# +---------------------+
+# |        Answer       | RRs answering the question
+# +---------------------+
+# |      Authority      | RRs pointing toward an authority
+# +---------------------+
+# |      Additional     | RRs holding additional information
+# +---------------------+
+#
+# header
+#
+#   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+# |                      ID                       |
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+# |QR|   Opcode  |AA|TC|RD|RA| (zero) |   RCODE   |
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+# |                    QDCOUNT                    |
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+# |                    ANCOUNT                    |
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+# |                    NSCOUNT                    |
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+# |                    ARCOUNT                    |
+# +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+
 GOOGLE_PUBLIC_DNS = ['8.8.8.8', '8.8.4.4',
                      '2001:4860:4860::8888', '2001:4860:4860::8844']
 
@@ -34,6 +66,45 @@ QTYPE_AAAA = 28
 QTYPE_AXFR = 252
 QTYPE_ANY = 255
 QCLASS_IN = 1
+QR_QUERY = 0
+QR_RESPONSE = 1
+
+
+class DNSResponse:
+    def __init__(self, data):
+        (self.transaction_id,
+         self.flags,
+         self.questions,
+         self.answer_prs,
+         self.authority_prs,
+         self.additional_prs) = struct.unpack('!HHHHHH', data[:12])
+        self._parse_flags()
+        q = self.questions
+        questions = []
+        pointer = 12
+        label_length = data[pointer]
+        while q > 0:
+            question = []
+            while label_length > 0:
+                pointer += 1
+                question.append(data[pointer:pointer+label_length])
+                pointer += label_length
+                label_length = data[pointer]
+            questions.append(b'.'.join(question))
+            q -= 1
+        print(questions)
+
+    def _parse_flags(self):
+        self.qr = (self.flags & 0x8000) >> 15
+        self.opcode = (self.flags & 0x7800) >> 11
+        self.aa = (self.flags & 0x0400) >> 10
+        self.tc = (self.flags & 0x0200) >> 9
+        self.rd = (self.flags & 0x0100) >> 8
+        self.ra = (self.flags & 0x0080) >> 7
+        self.rcode = self.flags & 0x000f
+
+    def __str__(self):
+        return '<{} {}>'.format(self.__class__.__name__, self.transaction_id)
 
 
 class DNSProtocol(asyncio.DatagramProtocol):
@@ -45,10 +116,11 @@ class DNSProtocol(asyncio.DatagramProtocol):
         self._qtype = qtype
         self._loop = asyncio.get_event_loop()
         self._transport = None
+        self.result = None
+        self._transaction_id = os.urandom(2)
 
     @property
     def _message(self):
-        request_id = os.urandom(2)
         header = struct.pack('!BBHHHH', 1, 0, 1, 0, 0, 0)
         address = self._hostname.strip('.')
         labels = address.split('.')
@@ -57,22 +129,23 @@ class DNSProtocol(asyncio.DatagramProtocol):
             label_length = len(label)
             if label_length > 63:
                 raise ValueError('Label length over 63.')
-            results.append(chr(label_length))
+            results.append(bytes(chr(label_length), 'UTF-8'))
             results.append(bytes(label, 'UTF-8'))
         results.append(b'\0')
         qtype_qclass = struct.pack('!HH', self._qtype, QCLASS_IN)
-        return request_id + header + b''.join(results) + qtype_qclass
+        return self._transaction_id + header + b''.join(results) + qtype_qclass
 
     def connection_made(self, transport):
         self._transport = transport
-        print('Send:', self._hostname)
         self._transport.sendto(self._message)
 
     def datagram_received(self, data, addr):
-        print("Received:", data.decode())
-
-        print("Close the socket")
         self._transport.close()
+        response = DNSResponse(data)
+        if response.transaction_id != int(self._transaction_id.hex(), 16):
+            raise IOError('mismatch transaction id')
+        print(response)
+        print(data)
 
     def error_received(self, exc):
         print('Error received:', exc)
@@ -85,15 +158,25 @@ class DNSProtocol(asyncio.DatagramProtocol):
 class DNSResolver:
     def __init__(self):
         self._loop = asyncio.get_event_loop()
+        self._parse_hosts()
+        self._parse_nameserver()
 
     @lru_cache(seconds=300)
-    async def resolve(self, hostname):
-        transport, protocol = await self._loop.create_datagram_endpoint(
-            lambda: DNSProtocol(hostname, QTYPE_A),
-            remote_addr=('8.8.8.8', 53)
-        )
-        self._loop.run_forever()
-        transport.close()
+    def resolve(self, hostname):
+        if hostname in self._hosts:
+            return self._hosts[hostname]
+        for server in self._servers:
+            connection = self._loop.create_datagram_endpoint(
+                lambda: DNSProtocol(hostname, QTYPE_AAAA),
+                remote_addr=(str(server), 53)
+            )
+            transport, protocol = self._loop.run_until_complete(connection)
+
+            self._loop.run_forever()
+            transport.close()
+            if protocol.result:
+                self._loop.close()
+                return protocol.result
         self._loop.close()
 
     def _parse_nameserver(self):
@@ -171,3 +254,8 @@ class DNSResolver:
                             self._hosts[hostname] = ip
         else:
             self._hosts['localhost'] = '127.0.0.1'
+
+
+if __name__ == '__main__':
+    resolver = DNSResolver()
+    print(resolver.resolve('www.baidu.com'))
